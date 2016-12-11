d67a91c6c5c49a494bc974ca00622c603eb4e37b
Mert Kaplan <mertkaplan.tr@gmail.com>
2016-12-12 00:06:16 +0300

    ard code

--- arduinobu/arduinobu.ino
+++ arduinobu/arduinobu.ino
@@ -25,6 +25,45 @@ void setup(void) {
   pinMode(LEDpinUc, OUTPUT);
   pinMode(LEDpinDort, OUTPUT);
   pinMode(LEDpinBes, OUTPUT);
+  digitalWrite(12,HIGH);
+  delay(1000);
+  digitalWrite(12,LOW);
+
+  digitalWrite(11,HIGH);
+  delay(1000);
+  digitalWrite(11,LOW);
+
+    digitalWrite(10,HIGH);
+  delay(1000);
+  digitalWrite(10,LOW);
+
+    digitalWrite(9,HIGH);
+  delay(1000);
+  digitalWrite(9,LOW);
+
+      digitalWrite(8,HIGH);
+  delay(1000);
+  digitalWrite(8,LOW);
+
+      digitalWrite(7,HIGH);
+  delay(1000);
+  digitalWrite(7,LOW);
+  
+delay(500);
+    digitalWrite(12,HIGH);
+        digitalWrite(11,HIGH);
+            digitalWrite(10,HIGH);
+                digitalWrite(9,HIGH);
+                  digitalWrite(8,HIGH);
+                  digitalWrite(7,HIGH);
+  delay(1500);
+  
+    digitalWrite(12,LOW);
+        digitalWrite(11,LOW);
+            digitalWrite(10,LOW);
+                digitalWrite(9,LOW);
+                  digitalWrite(8,LOW);
+  digitalWrite(7,LOW);
 }
  
 void loop(void) 
@@ -38,17 +77,17 @@ void loop(void)
   
   int led0=fsrReadingBir/2; 
   int led1=fsrReadingIki/4; 
-  int led2=fsrReadingUc/6;
-  int led3=fsrReadingDort/8;
-  int led4=fsrReadingBes/10;
-  int led5=fsrReadingAlti/12;
+  int led2=fsrReadingUc*3;
+  int led3=fsrReadingDort*4;
+  int led4=fsrReadingBes*5;
+  int led5=fsrReadingAlti*6;
 
   //BURANIN ALTINDAKI COMMENTLERI KALDIR!!!!!!!!!!!
  // Serial.print("Analog reading led0= "); 
-  Serial.println(led0);
+  //Serial.println(led0);
  // Serial.print("-");
   //Serial.print("Analog reading led1= "); 
-  Serial.println(led1);
+ 
  /* Serial.print("\n");
   Serial.print("Analog reading led2= "); 
   Serial.println(led2);
@@ -58,28 +97,98 @@ void loop(void)
    Serial.print("------------------------");
   Serial.print("\n");*/
   
-  
-  
-   if(led0 >= 480 || led0 <=520)
+
+ 
+   if(led0 >= 480 && led0 <=520)
    {
+   // Serial.println("LED 0'a dokundun: ");
+    Serial.println(led0);
     digitalWrite(12,HIGH);
    }
-   if(led0 <=480)
+   else
    {
     digitalWrite(12,LOW);
-   }
- /////////////////////LED0 ICIN CONDITION BITISI
+  }
+ 
+ /////////////////////LED 0 ICIN CONDITION BITISI
 
-    if(led1 >= 240 || led0 <=260)
+   
+    if(led1>=200 && led1<=260)
    {
+    //Serial.println("LED 1'e dokundun: ");
+    Serial.println(led1);
     digitalWrite(11,HIGH);
+     
    }
-   if(led1 <=240)
+   else
    {
     digitalWrite(11,LOW);
    }
- 
 
+   /////////////////////LED1 ICIN CONDITION BITISI
+
+       
+    if(led2>=2950 && led2<=3050)
+   {
+     Serial.println(led2);
+    digitalWrite(10,HIGH);
+     
+   }
+   else
+   {
+    digitalWrite(10,LOW);
+   }
+ /////////////////////LED2 ICIN CONDITION BITISI
+   
+   
+    if(led3>=3800 && led3<=4150)
+   {
+     Serial.println(led3);
+    digitalWrite(9,HIGH);
+     
+   }
+   else
+   {
+    digitalWrite(9,LOW);
+   }
+ /////////////////////LED2 ICIN CONDITION BITISI
+
+  
+   
+    if(led4>=4850 && led4<=5300)
+   {
+     Serial.println(led4);
+    digitalWrite(8,HIGH);
+     
+   }
+   else
+   {
+    digitalWrite(8,LOW);
+   }
+
+    if(led5>=6050 && led5<=6200)
+   {
+     Serial.println(led5);
+    digitalWrite(7,HIGH);
+     
+   }
+   else
+   {
+    digitalWrite(7,LOW);
+   }
+
+////////////////////////////
+if(Serial.available())
+{
+  int a=Serial.read();
+  if(a=='0')
+  {
+    digitalWrite(12,HIGH);
+    }
+    else
+    {digitalWrite(12,LOW);
+      }
+  }
 
    
   ///asil kod asagıda
@@ -87,6 +196,6 @@ void loop(void)
 //  LEDbrightness = map(num, 0, 1023, 0, 255);
   // LED gets brighter the harder you press
   //analogWrite(LEDpin, LEDbrightness);
- 
-  delay(1000);
+  delay(100);
+  //delay(1000);
 }
