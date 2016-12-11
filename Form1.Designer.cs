05b896d513a5ec3338ec4f861e4747d729e599fe
Mert Kaplan <mertkaplan.tr@gmail.com>
2016-12-11 19:35:51 +0300

    1day

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,29 +29,25 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.label1 = new System.Windows.Forms.Label();
             this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.btnBaglan = new System.Windows.Forms.Button();
             this.btnBaglantiKes = new System.Windows.Forms.Button();
+            this.lblArduinoAlinanDeger = new System.Windows.Forms.Label();
+            this.lblConnectionStatus = new System.Windows.Forms.Label();
+            this.picPianoPictures = new System.Windows.Forms.PictureBox();
+            this.picConnectionStatus = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picPianoPictures)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picConnectionStatus)).BeginInit();
             this.SuspendLayout();
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(488, 290);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(75, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "lblAlinanDeger";
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(41, 26);
+            this.textBox1.Location = new System.Drawing.Point(12, 580);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(316, 181);
+            this.textBox1.Size = new System.Drawing.Size(98, 20);
             this.textBox1.TabIndex = 1;
             // 
             // timer1
@@ -61,9 +57,9 @@
             // 
             // btnBaglan
             // 
-            this.btnBaglan.Location = new System.Drawing.Point(437, 70);
+            this.btnBaglan.Location = new System.Drawing.Point(387, 590);
             this.btnBaglan.Name = "btnBaglan";
-            this.btnBaglan.Size = new System.Drawing.Size(75, 23);
+            this.btnBaglan.Size = new System.Drawing.Size(52, 26);
             this.btnBaglan.TabIndex = 2;
             this.btnBaglan.Text = "Baglan";
             this.btnBaglan.UseVisualStyleBackColor = true;
@@ -71,39 +67,84 @@
             // 
             // btnBaglantiKes
             // 
-            this.btnBaglantiKes.Location = new System.Drawing.Point(532, 70);
+            this.btnBaglantiKes.Location = new System.Drawing.Point(445, 590);
             this.btnBaglantiKes.Name = "btnBaglantiKes";
-            this.btnBaglantiKes.Size = new System.Drawing.Size(75, 23);
+            this.btnBaglantiKes.Size = new System.Drawing.Size(52, 26);
             this.btnBaglantiKes.TabIndex = 3;
             this.btnBaglantiKes.Text = "Kes";
             this.btnBaglantiKes.UseVisualStyleBackColor = true;
             this.btnBaglantiKes.Click += new System.EventHandler(this.btnBaglantiKes_Click);
             // 
+            // lblArduinoAlinanDeger
+            // 
+            this.lblArduinoAlinanDeger.AutoSize = true;
+            this.lblArduinoAlinanDeger.Location = new System.Drawing.Point(12, 564);
+            this.lblArduinoAlinanDeger.Name = "lblArduinoAlinanDeger";
+            this.lblArduinoAlinanDeger.Size = new System.Drawing.Size(37, 13);
+            this.lblArduinoAlinanDeger.TabIndex = 4;
+            this.lblArduinoAlinanDeger.Text = "Value:";
+            // 
+            // lblConnectionStatus
+            // 
+            this.lblConnectionStatus.AutoSize = true;
+            this.lblConnectionStatus.Location = new System.Drawing.Point(282, 571);
+            this.lblConnectionStatus.Name = "lblConnectionStatus";
+            this.lblConnectionStatus.Size = new System.Drawing.Size(101, 13);
+            this.lblConnectionStatus.TabIndex = 0;
+            this.lblConnectionStatus.Text = "lblConnectionStatus";
+            // 
+            // picPianoPictures
+            // 
+            this.picPianoPictures.Image = global::ArduinoPiano.Properties.Resources._default;
+            this.picPianoPictures.Location = new System.Drawing.Point(-4, 1);
+            this.picPianoPictures.Name = "picPianoPictures";
+            this.picPianoPictures.Size = new System.Drawing.Size(650, 513);
+            this.picPianoPictures.TabIndex = 6;
+            this.picPianoPictures.TabStop = false;
+            // 
+            // picConnectionStatus
+            // 
+            this.picConnectionStatus.Image = global::ArduinoPiano.Properties.Resources.bekleniyor;
+            this.picConnectionStatus.Location = new System.Drawing.Point(389, 507);
+            this.picConnectionStatus.Name = "picConnectionStatus";
+            this.picConnectionStatus.Size = new System.Drawing.Size(108, 77);
+            this.picConnectionStatus.TabIndex = 5;
+            this.picConnectionStatus.TabStop = false;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(619, 390);
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(509, 628);
+            this.Controls.Add(this.picPianoPictures);
+            this.Controls.Add(this.picConnectionStatus);
+            this.Controls.Add(this.lblArduinoAlinanDeger);
             this.Controls.Add(this.btnBaglantiKes);
             this.Controls.Add(this.btnBaglan);
             this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblConnectionStatus);
             this.Name = "Form1";
-            this.Text = "s";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Istanbul Kultur University Piano Project";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picPianoPictures)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picConnectionStatus)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Label label1;
         private System.IO.Ports.SerialPort serialPort1;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.Button btnBaglan;
         private System.Windows.Forms.Button btnBaglantiKes;
+        private System.Windows.Forms.Label lblArduinoAlinanDeger;
+        private System.Windows.Forms.PictureBox picConnectionStatus;
+        private System.Windows.Forms.Label lblConnectionStatus;
+        private System.Windows.Forms.PictureBox picPianoPictures;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -11,6 +11,7 @@ using System.IO.Ports;
 using System.Media;
 using System.Threading;
 using System.Text.RegularExpressions;
+using System.IO;
 
 namespace ArduinoPiano
 {
@@ -19,9 +20,9 @@ namespace ArduinoPiano
         public Form1()
         {
             InitializeComponent();
-            
-     
-      
+
+
+
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -31,63 +32,144 @@ namespace ArduinoPiano
             serialPort1.Parity = System.IO.Ports.Parity.None;
             serialPort1.DataBits = 8;
             serialPort1.StopBits = System.IO.Ports.StopBits.One;
+            lblConnectionStatus.Text = "Waiting Action";
+            picConnectionStatus.Image = Properties.Resources.bekleniyor;
+            lblConnectionStatus.ForeColor = Color.Orange;
+        }
+
+        private void btnBaglan_Click(object sender, EventArgs e)
+        {
+
+            if (serialPort1.IsOpen == false)
+            {
+                serialPort1.Open();
+
+
+            }
+            if (serialPort1.IsOpen == true)
+            {
+                lblConnectionStatus.Text = "Connected";
+                picConnectionStatus.Image = Properties.Resources.acik;
+                lblConnectionStatus.ForeColor = Color.Green;
+                // connectArd();
+                //timer1.Start();
+                caldirFunc();
+            }
+
+        }
+
+     
+        void caldirFunc()
+        {
             
-
-            label1.Text = "Action bekleniyor";
-
-           
-
-
+            while (serialPort1.IsOpen == true)
+            {
+              //  string alinanDeger = serialPort1.ReadLine();
+                string value = serialPort1.ReadLine();
+                string[] lines = Regex.Split(value, "\r\n");
+                foreach (string line in lines)
+                {
+                    textBox1.Text = "Alinan Deger: " + value;
+                }
+                textBox1.Text = "Alinan Deger: " + value;
+                int gonderilendeger = int.Parse(value);
+                potansiyeller(gonderilendeger);
+            }
         }
 
-        void play1()
+
+      
+        void potansiyeller(int _donusturulendeger)
         {
-            System.Media.SoundPlayer player = new System.Media.SoundPlayer();
-            player.SoundLocation = "C:\\1.wav";
-            player.Play();
+        
+            int donusturulendeger = _donusturulendeger;
+            if (donusturulendeger >= 480 && donusturulendeger <= 520)
+            {
+               
+                picPianoPictures.Image = Properties.Resources._0;
+                donusturulendeger = 0;
+                play1();
+
+            }
+            else if (donusturulendeger >= 200 && donusturulendeger <= 260)
+            {
+             
+                play2();
+                picPianoPictures.Image = Properties.Resources._1;
+                donusturulendeger = 0;
+            }
+            else if (donusturulendeger >= 2950 && donusturulendeger <= 3050)
+            {
+                play3();
+                picPianoPictures.Image = Properties.Resources._2;
+                donusturulendeger = 0;
+            }
+            else if (donusturulendeger >= 3800 && donusturulendeger <= 4200)
+            {
+                play4();
+                picPianoPictures.Image = Properties.Resources._3;
+                donusturulendeger = 0;
+            }
+            else if (donusturulendeger >= 4850 && donusturulendeger <= 5300)
+            {
+                play5();
+                picPianoPictures.Image = Properties.Resources._4;
+                donusturulendeger = 0;
+            }
+            else if (donusturulendeger >= 6050 && donusturulendeger <= 6200)
+            {
+                picPianoPictures.Image = Properties.Resources._5;
+                play6();
+
+                donusturulendeger = 0;
+
+            }
+            else
+            {
+              
+                donusturulendeger = 0;
+                picPianoPictures.Image = Properties.Resources._default;
+            }
         }
 
-        void play2()
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    private void btnBaglantiKes_Click(object sender, EventArgs e)
         {
-            System.Media.SoundPlayer player = new System.Media.SoundPlayer();
-            player.SoundLocation = "C:\\2.wav";
-            player.Play();
+            if (serialPort1.IsOpen == true)
+            { serialPort1.Close();
+
+            }
+            if (serialPort1.IsOpen == false)
+            {
+                lblConnectionStatus.ForeColor = Color.Red;
+                picConnectionStatus.Image = Properties.Resources.kapali;
+                lblConnectionStatus.Text = "Disconnected";
+            }
         }
-
-        void play3()
-        {
-            System.Media.SoundPlayer player = new System.Media.SoundPlayer();
-            player.SoundLocation = "C:\\3.wav";
-            player.Play();
-        }
-
-        void play4()
-        {
-            System.Media.SoundPlayer player = new System.Media.SoundPlayer();
-            player.SoundLocation = "C:\\4.wav";
-            player.Play();
-        }
-
-        void play5()
-        {
-            System.Media.SoundPlayer player = new System.Media.SoundPlayer();
-            player.SoundLocation = "C:\\5.wav";
-            player.Play();
-        }
-
-        void play6()
-        {
-            System.Media.SoundPlayer player = new System.Media.SoundPlayer();
-            player.SoundLocation = "C:\\6.wav";
-            player.Play();
-        }
-
-
-
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-            
             try {
                 
                 string alinanDeger = serialPort1.ReadLine();
@@ -106,64 +188,84 @@ namespace ArduinoPiano
                 if (donusturulendeger >= 480 && donusturulendeger <= 520)
                 {
                     play1();
-
+                    picPianoPictures.Image = Properties.Resources._0;
+                    donusturulendeger = 0;
                 }
                 else if (donusturulendeger >= 200 && donusturulendeger <= 260)
-                { play2(); }
+                { play2();
+                    picPianoPictures.Image = Properties.Resources._1;
+                    donusturulendeger = 0;
+                }
                 else if (donusturulendeger >= 2950 && donusturulendeger <= 3050)
                 {
                     play3();
+                    picPianoPictures.Image = Properties.Resources._2;
+                    donusturulendeger = 0;
                 }
-                else if (donusturulendeger >= 3950 && donusturulendeger <= 4150)
+                else if (donusturulendeger >= 3800 && donusturulendeger <= 4200)
                 {
                     play4();
+                    picPianoPictures.Image = Properties.Resources._3;
+                    donusturulendeger = 0;
                 }
                 else if (donusturulendeger >= 4850 && donusturulendeger <= 5300)
                 {
                     play5();
+                    picPianoPictures.Image = Properties.Resources._4;
+                    donusturulendeger = 0;
                 }
                 else if (donusturulendeger >= 6050 && donusturulendeger <= 6200)
                 {
+                    picPianoPictures.Image = Properties.Resources._5;
                     play6();
+                    
+                    donusturulendeger = 0;
+                    
                 }
-                else { }
-
-
-
-
-
+                else { donusturulendeger = 0; picPianoPictures.Image = Properties.Resources._default;
+                   
+                }
             }
             catch (Exception ex)
             { textBox1.Text = ex.ToString();
                 timer1.Stop(); 
             }
-
         }
-
-        private void btnBaglan_Click(object sender, EventArgs e)
+        void play1()
         {
-            timer1.Enabled = true;
-            if (serialPort1.IsOpen == false)
-            {
-                serialPort1.Open();
-                
-               
-                
-            }
-            if(serialPort1.IsOpen==true)
-            {   
-                
-                label1.Text = "BAGLANTI ACIK"; }
-            
-
+            System.Media.SoundPlayer player = new System.Media.SoundPlayer();
+            player.SoundLocation = "C:\\1.wav";
+            player.Play();
         }
-
-        private void btnBaglantiKes_Click(object sender, EventArgs e)
+        void play2()
         {
-            if (serialPort1.IsOpen == true)
-            { serialPort1.Close(); }
-            if (serialPort1.IsOpen == false)
-            { label1.Text = "kestim"; }
+            System.Media.SoundPlayer player = new System.Media.SoundPlayer();
+            player.SoundLocation = "C:\\2.wav";
+            player.Play();
+        }
+        void play3()
+        {
+            System.Media.SoundPlayer player = new System.Media.SoundPlayer();
+            player.SoundLocation = "C:\\3.wav";
+            player.Play();
+        }
+        void play4()
+        {
+            System.Media.SoundPlayer player = new System.Media.SoundPlayer();
+            player.SoundLocation = "C:\\4.wav";
+            player.Play();
+        }
+        void play5()
+        {
+            System.Media.SoundPlayer player = new System.Media.SoundPlayer();
+            player.SoundLocation = "C:\\5.wav";
+            player.Play();
+        }
+        void play6()
+        {
+            System.Media.SoundPlayer player = new System.Media.SoundPlayer();
+            player.SoundLocation = "C:\\6.wav";
+            player.Play();
         }
     }
 }
