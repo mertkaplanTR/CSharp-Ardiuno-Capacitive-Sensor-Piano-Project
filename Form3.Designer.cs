bd15d15213014dceaa9a5b7ad36a40bdef4ea420
Mert Kaplan <mertkaplan.tr@gmail.com>
2016-12-13 05:13:18 +0300

    form 3

---
+++ Form3.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace ArduinoPiano
+{
+    partial class Form3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnCal = new System.Windows.Forms.Button();
+            this.btnBaglan = new System.Windows.Forms.Button();
+            this.picConnectionStatus = new System.Windows.Forms.PictureBox();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.picConnectionStatus)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnCal
+            // 
+            this.btnCal.Location = new System.Drawing.Point(146, 95);
+            this.btnCal.Name = "btnCal";
+            this.btnCal.Size = new System.Drawing.Size(75, 28);
+            this.btnCal.TabIndex = 17;
+            this.btnCal.Text = "Çal";
+            this.btnCal.UseVisualStyleBackColor = true;
+            this.btnCal.Click += new System.EventHandler(this.btnCal_Click);
+            // 
+            // btnBaglan
+            // 
+            this.btnBaglan.Location = new System.Drawing.Point(12, 95);
+            this.btnBaglan.Name = "btnBaglan";
+            this.btnBaglan.Size = new System.Drawing.Size(111, 26);
+            this.btnBaglan.TabIndex = 15;
+            this.btnBaglan.Text = "Baglan";
+            this.btnBaglan.UseVisualStyleBackColor = true;
+            this.btnBaglan.Click += new System.EventHandler(this.btnBaglan_Click);
+            // 
+            // picConnectionStatus
+            // 
+            this.picConnectionStatus.Image = global::ArduinoPiano.Properties.Resources.bekleniyor;
+            this.picConnectionStatus.Location = new System.Drawing.Point(15, 12);
+            this.picConnectionStatus.Name = "picConnectionStatus";
+            this.picConnectionStatus.Size = new System.Drawing.Size(108, 77);
+            this.picConnectionStatus.TabIndex = 14;
+            this.picConnectionStatus.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 216);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 18;
+            this.label1.Text = "label1";
+            // 
+            // Form3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(437, 343);
+            this.Controls.Add(this.btnCal);
+            this.Controls.Add(this.btnBaglan);
+            this.Controls.Add(this.picConnectionStatus);
+            this.Controls.Add(this.label1);
+            this.Name = "Form3";
+            this.Text = "Form3";
+            this.Load += new System.EventHandler(this.Form3_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picConnectionStatus)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnCal;
+        private System.Windows.Forms.Button btnBaglan;
+        private System.Windows.Forms.PictureBox picConnectionStatus;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ Form3.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ArduinoPiano
+{
+    public partial class Form3 : Form
+    {
+        public Form3()
+        {
+            InitializeComponent();
+        }
+
+        private void Form3_Load(object sender, EventArgs e)
+        {
+
+
+            picConnectionStatus.Image = Properties.Resources.bekleniyor;
+        }
+
+
+        private void btnBaglan_Click(object sender, EventArgs e)
+        {
+            serialPort1.BaudRate = 9600;
+            serialPort1.PortName = "COM8";
+            serialPort1.Parity = System.IO.Ports.Parity.None;
+            serialPort1.DataBits = 8;
+            serialPort1.StopBits = System.IO.Ports.StopBits.One;
+            serialPort1.Open();
+            if (serialPort1.IsOpen == false)
+            {
+                serialPort1.Open();
+            }
+            if (serialPort1.IsOpen == true)
+            {
+                picConnectionStatus.Image = Properties.Resources.acik;
+            }
+
+            runMusic();
+
+        }
+
+        void media(int _birinci, int _ikinci)
+        {
+            if(_birinci>200 && _ikinci>0)
+            { 
+            var player1 = new WMPLib.WindowsMediaPlayer();
+
+            player1.URL = @"C:\\1.wav";
+
+            var player2 = new WMPLib.WindowsMediaPlayer();
+
+            player2.URL = @"C:\\2.wav";
+                return;
+            }
+            else
+            {
+                var player1 = new WMPLib.WindowsMediaPlayer();
+                player1.URL = @"C:\\5.wav";
+            }
+
+
+        }
+
+
+
+        public void runMusic()
+        {
+           // int _whileCount = 0;
+            string alinanDegerBuyukMu = serialPort1.ReadLine();
+            string[] ayiriciArray = new string[] { "\r" };
+            string[] lines = alinanDegerBuyukMu.Split(ayiriciArray, StringSplitOptions.None);
+            alinanDegerBuyukMu = lines[0];
+            int karsilastiripArrayeAtilacakDeger = int.Parse(alinanDegerBuyukMu);
+
+            ////ALINAN DEGER 150'DEN BUYUKSE MIX ICIN ISLEMLERE BASLA
+            while (serialPort1.IsOpen == true && karsilastiripArrayeAtilacakDeger>150)
+            {
+                //HAFIZAYA ALINACAK BASMA SURELERINI BU ARRAYA GORE AYARLA
+                string[] arr = new string[2]; //10LUK STRING ARRAY TANIMLA  --> 300 ms*2=0,6 saniyelik 
+                for (int i = 0; i < 2; i++) //BASILI OLDUGU SURECE BUNLARI EGER 100'DEN BUYUKSE BURAYA GIRIP ARRAYA GONDER
+                {
+
+                    arr[i] = serialPort1.ReadLine();
+                 
+                }
+                //ARRAYA ALDIKTAN SONRA BUNLARI BIR INTEGER ARRAYA GONDER VE HEPSINI BERABER CALDIR.
+
+                foreach (string elemanlar in arr)
+                {
+                    if(elemanlar==null)
+                    { break; }
+                    else { 
+                    string arrayElamaniniIntDonustur = elemanlar;
+                    string[] donustur=arrayElamaniniIntDonustur.Split(ayiriciArray, StringSplitOptions.None);
+                    arrayElamaniniIntDonustur = donustur[0];
+                        ////BURASI ACILACAK//   
+                        Mixer(int.Parse(arrayElamaniniIntDonustur));
+                    }
+                    
+                }
+            }
+        }
+
+
+
+
+
+
+        int[] alinanDegerler = new int[2];
+        int countermixer=0;
+        void Mixer(int _alinanSayi)
+        {
+            if(countermixer>1)
+            {
+              countermixer = 0;
+            }
+            else if(countermixer<2)
+            { 
+            alinanDegerler[countermixer] = _alinanSayi;
+            }
+            if(countermixer==1)
+            {
+               if(alinanDegerler[0] > 0 && alinanDegerler[1] > 0)
+                {
+                    var player1 = new WMPLib.WindowsMediaPlayer();
+
+                    player1.URL = @"C:\\1.wav";
+
+                    var player2 = new WMPLib.WindowsMediaPlayer();
+
+                    player2.URL = @"C:\\2.wav";
+                }
+            }
+            countermixer++;
+        }
+
+        private void btnCal_Click(object sender, EventArgs e)
+        {
+           
+            Random r = new Random();
+            int n = r.Next();
+            Mixer(n);
+            
+        }
+    }
+}
